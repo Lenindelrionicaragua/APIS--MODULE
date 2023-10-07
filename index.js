f3b6965dcfe85d27a91f340207813b4d6dd449e9
Lenindelrionicaragua <lenindelrionicaragua@gmail.com>
2023-10-07 19:26:19 +0200

    add prep APIS week 1

---
+++ index.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const STEP_SIZE_PX = 10;
+const STEP_INTERVAL_MS = 50;
+const DANCE_TIME_MS = 5000;
+const DANCING_CAT_URL =
+  'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+const WALKING_CAT_URL =
+  'http://www.anniemation.com/clip_art/images/cat-walk.gif';
+
+function walk(img, startPos, stopPos) {
+  return new Promise((resolve) => {
+    let currentPosition = startPos;
+
+    const WalkInterval = setInterval(() => {
+      currentPosition += STEP_SIZE_PX;
+      img.style.left = currentPosition + 'px';
+
+      if (currentPosition >= stopPos) {
+        clearInterval(WalkInterval);
+        resolve();
+      }
+    }, STEP_INTERVAL_MS);
+  });
+}
+
+
+function dance(img) {
+  return new Promise((resolve) => {
+    const originalSrc = WALKING_CAT_URL;
+    img.src = DANCING_CAT_URL;
+
+    setTimeout(() => {
+      img.src = originalSrc;
+      resolve();
+    }, DANCE_TIME_MS);
+  });
+}
+
+function catWalk() {
+  const img = document.querySelector('img');
+  const startPos = -img.width;
+  const centerPos = (window.innerWidth - img.width) / 2;
+  const stopPos = window.innerWidth;
+
+
+  function animateCat() {
+    walk(img, startPos, centerPos)
+      .then(() => dance(img))
+      .then(() => walk(img, centerPos, stopPos))
+      .then(animateCat);
+  }
+
+  animateCat();
+}
+
+catWalk();
+
+window.addEventListener('load', catWalk);
